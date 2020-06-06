326765f11df3baaadf3959f8ddd825fecf71e2af
Cloter Gonçalves Barbosa Junior <cjunior-partner@bettatecnologia.com.br>
2020-06-06 10:41:01 -0300

    Aula dia 05 - Implementado upload de imagens

--- aulas/server/src/controllers/ItemsController.ts
+++ aulas/server/src/controllers/ItemsController.ts
@@ -11,7 +11,8 @@ class ItemsController {
                 title: item.title,
                 image_url: `http://192.168.15.13:3333/uploads/${item.image}`,
             };
-        })
+        });
+        
         return res.json(serializedItems);
     }
 }
